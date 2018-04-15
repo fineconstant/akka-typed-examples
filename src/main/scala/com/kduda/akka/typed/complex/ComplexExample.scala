9e15ac919e890b2b6f9d8de588ef17199407ce65
Kamil Duda <kamilduda01@gmail.com>
2018-04-15 16:54:21 +0200

    Add main for complex example

---
+++ src/main/scala/com/kduda/akka/typed/complex/ComplexExample.scala
@@ -0,0 +1,18 @@
+package com.kduda.akka.typed.complex
+
+import akka.actor.typed.ActorSystem
+import com.kduda.akka.typed.simple.actors.RootCoordinator
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+
+/**
+  * Create root actor
+  */
+object ComplexExample extends App {
+  val system = ActorSystem(RootCoordinator.coordinator, "ChatRoomDemo")
+  Await.result(system.whenTerminated, 3 seconds)
+}
+
